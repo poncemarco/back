7bb0fef01e1a3079431f8a0880f9b3b7d1a4a20f
marco_cap <maarco.app98@gmail.com>
2023-08-07 18:20:11 -0600

    upload

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to User application." });
 });
 
-require("./app/routes")(app);
+// require("./app/routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
